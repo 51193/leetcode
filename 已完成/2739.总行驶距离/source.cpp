5da9e8fce8821d41a196dc677b7b92e5975212f9
51193 <2878740067@qq.com>
2024-04-25 21:04:35 +0800

    2024/04/25

---
+++ 已完成/2739.总行驶距离/source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int distanceTraveled(int mainTank, int additionalTank)
+    {
+        return (mainTank + min((mainTank - 1) / 4, additionalTank)) * 10;
+    }
+};
